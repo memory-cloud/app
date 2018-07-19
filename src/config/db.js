ce72091f4d07fbf749f89a328b8cff70dafbf5fa
Rafael Santos <rafaelnsantos1@gmail.com>
2018-07-19 05:20:51 -0300

    retry mongodb connection

--- src/config/db.js
+++ src/config/db.js
@@ -1,38 +1,53 @@
 import Mongoose from 'mongoose'
 import cachegoose from 'cachegoose'
 import models from '@/models'
+
 const options = {
 	reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
 	reconnectInterval: 500, // Reconnect every 500ms
-	poolSize: 30, // Maintain up to 10 socket connections
-	// If not connected, return errors immediately rather than waiting for reconnect
-	bufferMaxEntries: 0
+	poolSize: 50, // Maintain up to 10 socket connections
+	connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
 }
 
-Mongoose.connect(process.env.MONGODB_URI, options)
-Mongoose.set('debug', true)
-
-Mongoose.set('debug', function (coll, method, query, doc, options) {
-	let set = {
-		coll: coll,
-		method: method,
-		query: query,
-		doc: doc,
-		options: options
-	}
-
-	console.log({
-		dbQuery: set
+function startDB () {
+	Mongoose.connect(process.env.MONGODB_URI, options, function (error) {
+		if (error) {
+			startDB()
+		} else {
+			// debug()
+			Object.keys(models).map(model => require('@/models/' + model))
+			setupRedis()			
+		}
 	})
-})
+}
 
-Object.keys(models).map(model => require('@/models/' + model))
+function debug () {
+	Mongoose.set('debug', true)
 
-cachegoose(Mongoose, {
-	engine: 'redis', /* If you don't specify the redis engine,      */
-	port: process.env.REDIS_PORT, /* the query results will be cached in memory. */
-	host: process.env.REDIS_HOST,
-	compress: true
-})
+	Mongoose.set('debug', function (coll, method, query, doc, options) {
+		let set = {
+			coll: coll,
+			method: method,
+			query: query,
+			doc: doc,
+			options: options
+		}
+
+		console.log({
+			dbQuery: set
+		})
+	})
+}
+
+function setupRedis () {
+	cachegoose(Mongoose, {
+		engine: 'redis', /* If you don't specify the redis engine,      */
+		port: process.env.REDIS_PORT, /* the query results will be cached in memory. */
+		host: process.env.REDIS_HOST,
+		compress: true
+	})
+}
+
+startDB()
 
 module.exports = Mongoose
