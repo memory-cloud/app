43fbbdc3e0b5ee84fb8f1207eb2e5797031575e3
Rafael Santos <rafealnsantos1@gmail.com>
2018-07-25 18:44:33 -0300

    test login google

--- src/graphql/test/resolver.js
+++ src/graphql/test/resolver.js
@@ -1,37 +1,17 @@
-// const { Issuer } = require('openid-client')
-var jwt = require('jsonwebtoken')
+import { OAuth2Client } from 'google-auth-library'
 
 exports.resolver = {
 	Query: {
-		async Test (db, {idtoken, clientid, clientsecret}) {
+		async Test (db, {idtoken, clientid, clientsecret, redirecturi}) {
 			try {
-				// check token before
-				var user = jwt.decode(idtoken)
-				console.log(user)
-				// issuer = Issuer.discover('https://accounts.google.com') // => Promise
-				// 	.then(function (googleIssuer) {
-				// 		const client = new googleIssuer.Client({
-				// 			client_id: clientid,
-				// 			client_secret: clientsecret
-				// 		})
-				// 		client.userinfo(idtoken) // => Promise
-				// 			.then(function (userinfo) {
-				// 				console.log('userinfo %j', userinfo)
-				// 			})
-				// 	})
-
-				// const client = new OAuth2Client(clientid, clientsecret)
-				//
-				// const ticket = await client.verifyIdToken({
-				// 	idToken: idtoken,
-				// 	audience: clientid
-				// })
-				// const payload = ticket.getPayload()
-				// console.log(payload)
-				// const userid = payload['sub']
-				// // If request specified a G Suite domain:
-				// // const domain = payload['hd'];
-				// return userid
+				const client = new OAuth2Client(clientid, clientsecret, redirecturi)
+				let ticket = await client.verifyIdToken({
+					idToken: idtoken,
+					audience: clientid
+				})
+				const payload = ticket.getPayload()
+				const userid = payload['sub']
+				return userid
 			} catch (err) {
 				return err
 			}
